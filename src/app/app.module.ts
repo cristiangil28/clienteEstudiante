2d504f7fe4361f54183af879b37823bcb2e84a27
cristian gil <cristianingeniero2018@hotmail.com>
2023-02-24 12:24:02 -0500

    crear los componentes estudiante, explore, header, las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EstudianteComponent } from './components/estudiante/estudiante.component';
+import { ExploreComponent } from './components/explore/explore.component';
+import { HeaderComponent } from './components/header/header.component';
+import {Routes} from '@angular/router';
+const appRoutes: Routes = [
+  {path:'', component: ExploreComponent},
+  {path: 'estudiante', component: EstudianteComponent}
+  {path: 'header', component: HeaderComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudianteComponent,
+    ExploreComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

2577a7fd4b5c71cc70db8a6db66a2ef6d1b934e2
cristian gil <cristianingeniero2018@hotmail.com>
2023-06-30 11:26:04 -0500

    Validar campos del formulario crud estudiantes

--- src/app/components/add-estudiante/add-estudiante.component.ts
+++ src/app/components/add-estudiante/add-estudiante.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EstudianteServiceService } from 'src/app/servicios/estudiante-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-estudiante',
   templateUrl: './add-estudiante.component.html'
 })
 export class AddEstudianteComponent {
-  constructor(private formBuilder: FormBuilder, private router: Router, private service: EstudianteServiceService){
-    
+  constructor(private formBuilder: FormBuilder, private router: Router, private service: EstudianteServiceService) {
+
   }
   addForm: any;
   ngOnInit() {
@@ -23,10 +24,27 @@ export class AddEstudianteComponent {
     });
   }
   onSubmit() {
-    this.service.createEstudiante( this.addForm.value )
-      .subscribe(data => {
-        this.router.navigate(['/']);
-        
-      });
+    if (this.addForm.valid) {
+      this.service.createEstudiante(this.addForm.value)
+        .subscribe(data => {
+          this.router.navigate(['/']);
+
+        });
+      Swal.fire({
+        title: 'Exitosamente',
+        text: 'Se ha guardado la materia',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      })
+    } else {
+
+      Swal.fire({
+        title: 'Error!',
+        text: 'Faltan Datos',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    }
+
   }
 }
--- src/app/components/edit-estudiante/edit-estudiante.component.ts
+++ src/app/components/edit-estudiante/edit-estudiante.component.ts
@@ -3,26 +3,27 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Estudiante } from 'src/app/modelos/estudiante';
 import { EstudianteServiceService } from 'src/app/servicios/estudiante-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-estudiante',
   templateUrl: './edit-estudiante.component.html'
 })
 export class EditEstudianteComponent {
-  estudiante : any;
+  estudiante: any;
   addForm: any;
   id: any;
-  constructor(private activeRouter: ActivatedRoute,private formBuilder: FormBuilder, private router: Router, private service: EstudianteServiceService){
+  constructor(private activeRouter: ActivatedRoute, private formBuilder: FormBuilder, private router: Router, private service: EstudianteServiceService) {
     this.estudiante = new Estudiante();
   }
-  
-  
+
+
   ngOnInit() {
-    if(this.activeRouter.snapshot.paramMap.get('id') != null){
+    if (this.activeRouter.snapshot.paramMap.get('id') != null) {
       this.id = this.activeRouter.snapshot.paramMap.get('id');
       this.service.getEstudiante(parseInt(this.id)).subscribe(data => this.estudiante = data);
     }
-      
+
     this.addForm = this.formBuilder.group({
       id: [],
       nombre: ['', Validators.required],
@@ -32,15 +33,29 @@ export class EditEstudianteComponent {
       tipoUsuario: ['', Validators.required],
       documento: ['', Validators.required]
     });
-    
+
     console.log(this.activeRouter.snapshot.paramMap.get('id'));
   }
   onSubmit() {
-    console.log(this.addForm.value.id);
-    this.service.updateEstudiante( this.addForm.value.id,this.addForm.value )
-      .subscribe(data => {
-        this.router.navigate(['/']);
-        
-      });
+
+    if (this.addForm.valid) {
+      this.service.updateEstudiante(this.addForm.value.id, this.addForm.value)
+        .subscribe(data => {
+          this.router.navigate(['/']);
+        });
+      Swal.fire({
+        title: 'Exitosamente',
+        text: 'Se ha guardado la materia',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      })
+    } else {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Faltan Datos',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    }
   }
 }
